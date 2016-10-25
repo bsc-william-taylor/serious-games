18931dfd75ea45ca0064cc907b2d6090c0cd83f3
William Taylor <wi11berto@yahoo.co.uk>
2016-10-25 16:20:40 +0100

    adding tinu's building model stripping away assets that are not needed

--- Assets/Scripts/PlayOnClick.cs
+++ Assets/Scripts/PlayOnClick.cs
@@ -4,23 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class PlayOnClick : MonoBehaviour
 {
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
     public void OnMouseDown()
     {
         SceneManager.LoadScene(1);
-
-        Debug.Log("I was hit");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
