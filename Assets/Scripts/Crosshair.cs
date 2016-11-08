6746f72675ed4d2f6d2d7055ef68d1bee3085781
tynutsu <tynutsu@yahoo.com>
2016-11-08 01:54:37 +0000

    moved textures under Resources added crosshair for scenarios

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+
+    const float DEFAULT_SIZE = 5.0f;
+
+    // crosshair's percentage compared to the screen width
+    public float size = DEFAULT_SIZE;
+    Rect crosshairSurface;
+    Texture crosshairTexture;
+
+	// Use this for initialization
+	void Start () {
+        crosshairTexture = Resources.Load("Textures/crosshair") as Texture;
+        
+        float crosshairLength = Screen.width * size / 100.0f;
+
+        crosshairSurface = new Rect(
+            Screen.width / 2.0f - crosshairLength / 2.0f,
+            Screen.height/2.0f - crosshairLength /2.0f,
+            crosshairLength,
+            crosshairLength);
+
+    }
+	
+	void OnGUI () {
+        GUI.DrawTexture(crosshairSurface, crosshairTexture);
+	}
+}
