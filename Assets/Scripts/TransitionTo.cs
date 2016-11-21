bb356b817cf6f2924c57efb631cb5add65635ed4
William Taylor <wi11berto@yahoo.co.uk>
2016-11-21 13:13:12 +0000

    basic ui scenes will start adding video

---
+++ Assets/Scripts/TransitionTo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TransitionTo : MonoBehaviour
+{
+    public int SceneID = 0;
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene(SceneID);
+    }
+}
