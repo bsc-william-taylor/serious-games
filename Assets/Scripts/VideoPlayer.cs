b64a34cb6d101208f28cb4eaff5057aa6e3b7544
William Taylor <wi11berto@yahoo.co.uk>
2016-11-21 13:43:38 +0000

    adding imporved video player

--- Assets/Scripts/VideoPlayer.cs
+++ Assets/Scripts/VideoPlayer.cs
@@ -1,36 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System;
 
 [RequireComponent(typeof(AudioSource))]
-public class VideoPlayer : MonoBehaviour
+public class VideoPlayer : MonoBehaviour, IPointerClickHandler
 {
     public MovieTexture movTexture;
+    public bool PlayOnLoad;
 
+    private AudioSource audioSource;
+    
     void Start()
     {
-        var componentRenderer = GetComponent<Renderer>();
-        var audioSource = GetComponent<AudioSource>();
+        var rawImage = GetComponent<RawImage>();
+        audioSource = GetComponent<AudioSource>();
 
-        if (componentRenderer != null && audioSource != null)
+        if (rawImage != null && audioSource != null)
         {
-            componentRenderer.material.mainTexture = movTexture;
+            rawImage.texture = movTexture;
             audioSource.clip = movTexture.audioClip;
-      
-            audioSource.Play();
-            movTexture.Play();
 
+            if (PlayOnLoad)
+            {
+                audioSource.Play();
+                movTexture.Play();
+            }
+           
             StartCoroutine(WaitForMovie(movTexture));
         }
         else
         {
-            Debug.LogError("Error Renderer || AudioSource is null");
+            Debug.LogError("RawImage is null || AudioSource is null");
         }
     }
 
     IEnumerator WaitForMovie(MovieTexture texture)
     {
         yield return new WaitForSeconds(texture.duration);
-        
-        // ... Handle when the texture is finished   
+
+        audioSource.Stop();
+        movTexture.Stop();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!movTexture.isPlaying)
+        {
+            audioSource.Play();
+            movTexture.Play();
+        }
+        else
+        {
+            audioSource.Pause();
+            movTexture.Pause();
+        }
+       
     }
 }
